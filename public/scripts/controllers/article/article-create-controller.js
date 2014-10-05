5cc24ce2a4ab94c889b9cc4425e138fd28048d48
Димо Петров <webdude@webdude.eu>
2014-10-05 23:14:00 +0300

    I call it a night

--- public/scripts/controllers/article/article-create-controller.js
+++ public/scripts/controllers/article/article-create-controller.js
@@ -1,4 +1,22 @@
 'use strict';
 meanBlog.controller('ArticleCreateController', function ArticleCreateController($scope, ArticleResource, CategoryResource) {
     $scope.categories = CategoryResource.getAllCategories();
+    $scope.createNew = function(article){
+
+    };
+
+    function isEmpty(str) {
+        return (!str || 0 === str.length || str.trim());
+    }
+
+    $scope.formIsInvalid = function(article){
+        if (isEmpty(article.title)){
+            return true;
+        }
+
+        if (isEmpty(article.body)){
+            return true;
+        }
+    }
+
 });
