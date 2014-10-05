f35e17975be8954e34e4fde8817e080befedf1c3
Димо Петров <webdude@webdude.eu>
2014-10-05 17:24:35 +0300

    s
    s

--- server/view-models/article-view-model.js
+++ server/view-models/article-view-model.js
@@ -3,21 +3,22 @@ var Article = mongoose.model('Article');
 var User = mongoose.model('User');
 var Category = mongoose.model('Category');
 
-function setCategoryNames(dataCategories, object){
-    object.categories = [];
-    dataCategories.forEach(function(category){
-        object.categories.push(category.title);
-    });
-}
-
 function ArticleViewModel(article) {
     this.title = article.title;
-    this.author = article.author.username;
-    setCategoryNames(article.categories, this);
+    this.author = {
+        username: article.author.username,
+        id: article.author._id
+    };
+    this.category = {
+        title: article.category.title,
+        id: article.category._id
+    };
     this.body = article.body;
     this.comments = article.comments;
     this.date = article.date;
     this.meta = article.meta;
+
+    return this;
 }
 
 module.exports = {
