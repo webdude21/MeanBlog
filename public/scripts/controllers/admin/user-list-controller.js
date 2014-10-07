47b6161323be4675482845b4bb3960a1080e8fde
pirinit <pirin.karabenchev@gmail.com>
2014-10-08 01:32:48 +0300

    Pager added

--- public/scripts/controllers/admin/user-list-controller.js
+++ public/scripts/controllers/admin/user-list-controller.js
@@ -6,7 +6,17 @@ meanBlog.controller('UserListController', function($scope, $http, notifier) {
                 $scope.users = gridResponse.data;
                 $scope.pager = gridResponse.pager;
                 $scope.sort = gridResponse.sort;
+                var pages = [];
+                for(var i = 1; i<=$scope.pager.totalPages; i++){
+                    var page = { number: i};
+                    if(i == $scope.pager.currentPage){
+                        page.class = 'active';
+                    }
 
+                    pages.push(page);
+                }
+
+                $scope.pages = pages;
                 $scope.columns.forEach(function (column) {
                     if (column.name == $scope.sort.columnName) {
                         column.class = $scope.sort.order;
@@ -29,26 +39,26 @@ meanBlog.controller('UserListController', function($scope, $http, notifier) {
         { name: "lastName", label: 'Last name'},
         { name: "username", label: 'Username'}
     ];
+
+    // actions
     $scope.headerClick = function(column){
+        var newClass = '';
         switch(column.class){
             case 'desc':
-                column.class = 'asc';
+                newClass = 'asc';
                 break;
             default :
                 // desc is the default sort order
-                column.class = 'desc';
+                newClass = 'desc';
                 break;
         }
-        var gridRequest = {
-            pager: {
-                currentPage: 1
-            },
-            sort:{
-                columnName: column.name,
-                order: column.class
-            }
-        };
-        getUsersList(gridRequest);
+
+        $scope.columns.forEach(function(column){
+            column.class = undefined;
+        });
+
+        column.class = newClass;
+        refreshData();
     };
     $scope.getHeaderCssClass = function (sortOrder){
         var cssClass = '';
@@ -63,5 +73,40 @@ meanBlog.controller('UserListController', function($scope, $http, notifier) {
 
         return cssClass;
     };
+    $scope.pagerClick = function(page){
+        $scope.pages.forEach(function(page){
+            page.class = undefined;
+        });
+
+        page.class = 'active';
+        refreshData();
+    }
+
+    function refreshData(){
+        var gridRequest = {
+            pager:{
+                currentPage:1
+            },
+            sort:{
+                columnName: "username",
+                "order":"asc"
+            }
+        }
+        $scope.pages.forEach(function(page){
+            if(page.class == 'active'){
+                gridRequest.pager.currentPage = page.number;
+                return false;
+            }
+        });
+
+        $scope.columns.forEach(function(column){
+            if(column.class != undefined){
+                gridRequest.sort.columnName = column.name;
+                gridRequest.sort.order = column.class;
+            }
+        });
+
+        getUsersList(gridRequest);
+    }
 });
 
