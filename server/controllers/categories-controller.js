68903034e58f06a8e455c94d8d0fa33d0d585070
kizisoft <kizisoft@gmail.com>
2014-10-09 02:20:26 +0300

    Start category edit controller

--- server/controllers/categories-controller.js
+++ server/controllers/categories-controller.js
@@ -4,7 +4,7 @@ var Category = mongoose.model('Category');
 var CANNOT_LIST_CATEGORIES = 'Cannot list the categories';
 
 module.exports = {
-    all: function (request, response, next) {
+    all: function (request, response) {
         Category.find().exec(function (error, results) {
             if (error) {
                 return response.json(500, {
