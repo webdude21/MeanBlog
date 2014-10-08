a59916e04cfaff878c70f92b5ed2bb9dc0e6a62d
Димо Петров <webdude@webdude.eu>
2014-10-08 21:44:38 +0300

    sf
    sf

--- public/scripts/controllers/article/article-detail-controller.js
+++ public/scripts/controllers/article/article-detail-controller.js
@@ -1,12 +1,37 @@
 'use strict';
 meanBlog.controller('ArticleDetailController',
     function ArticleDetailController($scope, identity, CommentResource, $routeParams, ArticleResource) {
-    $scope.identity = identity;
-    $scope.article = ArticleResource.getById({id: $routeParams.id});
-    $scope.comments = CommentResource.query({
-        orderBy: 'title',
-        orderType: 'false',
-        page: 1,
-        article: $routeParams.id
+        $scope.identity = identity;
+        $scope.commentsPage = 1;
+        $scope.article = ArticleResource.getById({id: $routeParams.id});
+        $scope.previousButtonEnabled = true;
+        $scope.nextButtonEnabled = true;
+
+        var queryObject = {
+            orderBy: 'date',
+            orderType: 'true',
+            page: $scope.commentsPage,
+            article: $routeParams.id
+        };
+        $scope.comments = CommentResource.query(queryObject);
+
+        $scope.older = function () {
+            $scope.commentsPage += 1;
+            $scope.comments = CommentResource.query({
+                orderBy: 'date',
+                orderType: 'true',
+                page: $scope.commentsPage,
+                article: $routeParams.id
+            });
+        };
+
+        $scope.newer = function () {
+            $scope.commentsPage -= 1;
+            $scope.comments = CommentResource.query({
+                orderBy: 'date',
+                orderType: 'true',
+                page: $scope.commentsPage,
+                article: $routeParams.id
+            });
+        }
     });
-});
