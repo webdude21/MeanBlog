b46a6cc5c355aaee2e66f49943b4ebac4372e611
kizisoft <kizisoft@gmail.com>
2014-10-09 02:20:35 +0300

    Start category edit controller

--- public/scripts/controllers/category/categories-controller.js
+++ public/scripts/controllers/category/categories-controller.js
@@ -1,8 +1,11 @@
 'use strict';
-meanBlog.controller('CategoriesController', function CategoriesController($scope, $http, notifier) {
+meanBlog.controller('CategoriesController', function CategoriesController($scope, $http, identity, notifier) {
+    var ERROR_READING_CATEGORY = 'Cannot read categories!';
+
+    $scope.identity = identity;
     $scope.categories = $http.get('/api/categories/').success(function (result) {
         $scope.categories = result;
-    }).error(function (error) {
-        notifier.error(error);
+    }).error(function () {
+        notifier.error(ERROR_READING_CATEGORY);
     });
 });
