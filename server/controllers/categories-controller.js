d6d8e2de590617270fc17b67f174b7ba17e84b01
kizisoft <kizisoft@gmail.com>
2014-10-08 10:28:39 +0300

    Category controller - only prove that works 2

--- server/controllers/categories-controller.js
+++ server/controllers/categories-controller.js
@@ -19,7 +19,7 @@ module.exports = {
             res.json(resultCategories);
         });
     },
-    getCategoryById: function () {
+    getCategoryById: function (req, res) {
 
     },
     createNew: function (req, res) {
