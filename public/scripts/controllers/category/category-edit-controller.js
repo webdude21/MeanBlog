b76c1a3fe190197aa549d543b8f8ff6e8f392731
kizisoft <kizisoft@gmail.com>
2014-10-09 14:58:11 +0300

    Start category edit controller ++

--- public/scripts/controllers/category/category-edit-controller.js
+++ public/scripts/controllers/category/category-edit-controller.js
@@ -6,6 +6,7 @@ meanBlog.controller('CategoryEditController', function CategoryEditController($s
     if ($routeParams.id && $routeParams.id !== 'null') {
         $http.get(CATEGORY_API_PATH + $routeParams.id).success(function (result) {
             $scope.category = result[0];
+            $scope.date = dateToInputString(result[0].date);
             $scope.edit = true;
         }).error(function () {
             notifier.error(ERROR_READING_CATEGORY);
@@ -14,3 +15,18 @@ meanBlog.controller('CategoryEditController', function CategoryEditController($s
         $scope.edit = false;
     }
 });
+
+function pad(number) {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
+
+function dateToInputString(dateFromDB) {
+    var date = new Date(dateFromDB);
+
+    return   date.getUTCFullYear() +
+        '-' + pad(date.getUTCMonth() + 1) +
+        '-' + pad(date.getUTCDate());
+}
