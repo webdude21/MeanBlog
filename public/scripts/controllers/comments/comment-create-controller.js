a0b8a1ac24798c35ca8c2756282a5c0cf67b798b
Димо Петров <webdude@webdude.eu>
2014-10-09 11:47:48 +0300

    s
    s

--- public/scripts/controllers/comments/comment-create-controller.js
+++ public/scripts/controllers/comments/comment-create-controller.js
@@ -1,10 +1,19 @@
 'use strict';
 meanBlog.controller('CommentCreateController',
-    function CommentCreateController($scope, identity, CommentResource, $routeParams, ArticleResource) {
+    function CommentCreateController($scope, notifier, identity, $location, CommentResource, $routeParams, ArticleResource) {
         $scope.identity = identity;
         $scope.article = ArticleResource.getById({id: $routeParams.articleId});
+        var COMMENT_CREATE_SUCCESS = 'You comment was sent successfully!';
 
-        $scope.create
-
-
+        $scope.createNew = function (comment) {
+            comment.article = $routeParams.articleId;
+            CommentResource.save(comment, function (response) {
+                if (response.$resolved) {
+                    notifier.success(COMMENT_CREATE_SUCCESS);
+                    $location.path("/articles/" + response._id);
+                } else {
+                    notifier.error("Your comment cannot be created!")
+                }
+            });
+        }
     });
