61e572db5db745d59321a64d0d822b714bd0e22b
Димо Петров <webdude@webdude.eu>
2014-10-09 14:40:16 +0300

    Home now lists the 10 most recent articles

--- public/scripts/controllers/home-controller.js
+++ public/scripts/controllers/home-controller.js
@@ -1,4 +1,5 @@
 'use strict';
-meanBlog.controller('HomeController', function HomeController($scope, identity) {
+meanBlog.controller('HomeController', function HomeController($scope, ArticleResource, identity) {
     $scope.identity = identity;
+    $scope.articles = ArticleResource.query();
 });
